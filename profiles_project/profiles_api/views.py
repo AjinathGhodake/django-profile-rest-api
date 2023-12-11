2c94d873a427171b98ac81e888f0766e2a59434c
ajinath ghodake <aj@192.168.1.4>
2023-12-11 16:46:22 +0530

    added different methids apis

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloAPIView(APIView):
     """Test API View"""
+
+    serializers_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """returns a list of APIView features"""
@@ -14,3 +19,26 @@ class HelloAPIView(APIView):
             "Is mapped manually to URLs",
         ]
         return Response({"message": "Hello!", "an_apiview": an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializers = self.serializers_class(data=request.data)
+
+        if serializers.is_valid():
+            name = serializers.validated_data.get("name")
+            message = f"hello {name}"
+            return Response({"message": message})
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handle updating object"""
+        return Response({"Method": "PUT"})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({"Method": "Patch"})
+
+    def delete(self, request, pk=None):
+        """Delete an Object"""
+        return Response({"Method": "delete"})
